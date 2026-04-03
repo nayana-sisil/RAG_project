b108902c1dfda0debb6d827d364bacbc0b822993
nayana sisil <nayanasisil@gmail.com>
2026-04-03 08:12:25 +0530

    add retriver

--- qabot.py
+++ qabot.py
@@ -27,6 +27,44 @@ def get_llm():
     return llm
 
 
+def document_loader(file):
+    loader = PyPDFLoader(file.name)
+    loaded_document = loader.load()
+    return loaded_document
+    
+
+def text_splitter(data):
+    text_splitter = RecursiveCharacterTextSplitter(
+        chunk_size=1000,
+        chunk_overlap=200,
+        length_function=len,
+    )
+    chunks = text_splitter.split_documents(data)
+    return chunks
+
+
+def hf_embedding():
+    embeddings = HuggingFaceEmbeddings(
+        model_name="sentence-transformers/all-MiniLM-L6-v2"
+    )
+    return embeddings
+
+
+def vector_database(chunks):
+    embedding_model = hf_embedding()
+    vectordb = Chroma.from_documents(chunks, embedding_model)
+    return vectordb
+
+
+def retriever(file):
+    splits = document_loader(file)
+    chunks = text_splitter(splits)
+    vectordb = vector_database(chunks)
+    retriever = vectordb.as_retriever()
+    return retriever
+
+
+
 
 
 
